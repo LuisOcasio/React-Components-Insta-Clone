e2f350a269e2991e9bc740897d95f468479f269a
luis ocasio <luisocasio@luiss-MacBook-Pro.local>
2020-03-18 10:26:58 -0400

    ui displayed- need to implement like functionality

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,22 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
+import data from "../../dummy-data";
 
-const PostsPage = () => {
+const PostsPage = props => {
   // set up state for your data
+  const [posts] = useState(data);
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {posts.map(e => (
+        <Post key={e.id} post={e} />
+      ))}
     </div>
   );
 };
 
 export default PostsPage;
-
